0a707005b36eddf43e2c9d08abbcbedb8ced64d3
Tejeshwer <special.account@gmail.com>
2017-07-06 01:03:04 +1000

    Update util.js

--- util.js
+++ util.js
@@ -1,1 +1,2 @@
-$("<div>Request:<br><textarea id='request'></textarea><br>Response:<br><textarea id='response'></textarea><br><button id='Execute'>Execute</button></div>").dialog();$("#Execute").click(function(){$('#response').val(eval($('#request').val()));});
+$("<div>Request:<br><textarea id='request'></textarea><br>Response:<br><textarea id='response'></textarea><br><button id='Execute'>Execute</button></div>").dialog();$("#Execute").click(function(){eval($('#request').val());});console.log = function(str) {$("#response").val(str);}
+function (str) {$("#response").val(str);}
