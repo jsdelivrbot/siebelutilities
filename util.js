849b4606f54f957818371534fb94733d95d409ce
Tejeshwer <special.account@gmail.com>
2017-07-13 23:04:24 +1000

    Update util.js

--- util.js
+++ util.js
@@ -1,9 +1,15 @@
 $("#console_wrapper").remove();
 $("<div id='console_wrapper'>Request:<br><textarea id='request' rows='12' cols='90'></textarea><br>" +
-  "<button id='ClearRespose'>Clear</button><button id='Execute'>Execute JS</button>" +
+  "<button id='ClearReq'>Clear</button><button id='Execute'>Execute JS</button>" +
   "<button id='ExecuteServer'>Execute eScript</button><br>Response:<br>" +
   "<textarea rows='12' cols='90' id='response' rows='12' cols='90'></textarea>" +
   "<button id='ClearRespose'>Clear</button></div>").dialog({width:'auto'});
+$("#ClearReq").click(function(){
+$('#request').val("");
+});
+$("#ClearRespose").click(function(){
+$("#response").val("");
+});
 $("#Execute").click(function(){
   try{
   eval($('#request').val());
